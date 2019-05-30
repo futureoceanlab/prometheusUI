429c01ea3e117b8814830f515ca335a169cceb9a
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-05-30 03:43:16 -0400

    hi junsu

--- uiFunctionCalls.py
+++ uiFunctionCalls.py
@@ -2,15 +2,16 @@
 def capturePhotoCommand2D(filename):
 	#
 def capturePhotoCommand3D(filename):
-
+	#
 def change2dExposure(exposure):
-
+	#
 def change3dExposure(exposure):
-
+	#
 def toggle2d3dMode(mode):
-
+	#
 def setModulationFrequency(freq):
-
+	#
 def enablePiDelay(enable):
-
+	#
 def enableCapture(enable):
+	#
