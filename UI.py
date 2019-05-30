4edc2a38d8f62023e7a611a20c0e1eb0767569e7
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-05-30 01:50:27 -0400

    added readBinaryFile file

--- UI.py
+++ UI.py
@@ -649,17 +649,6 @@ class Application(tk.Frame):
 	def change_title(self, newTitle):
 		self.title = newTitle 
 
-	def readBinaryFile(filepath):
-		numBytes = os.path.getsize(filepath)	#size in bytes
-		numPixels = int(numBytes/2)
-		numFrames = int(numPixels/(320*240))
-		numBytesPerFrame = int((numPixels/numFrames))
-		file = open(filepath, 'r')
-		array = np.fromfile(file, dtype=np.uint16)
-		for i in range(0,numFrames):
-			newArray = array[i*numBytesPerFrame:(i+1)*numBytesPerFrame].reshape(320,240)
-			img = Image.fromarray(newArray, mode='L')
-			img.save("./generatedImages/newImage"+str(i)+".jpg", 'JPEG')
 
 
 
