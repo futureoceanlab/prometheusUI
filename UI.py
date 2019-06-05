beff75ccad4224fe95767366521742faafb4ac3f
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-05 18:11:34 -0400

    cleaning up code

--- UI.py
+++ UI.py
@@ -501,10 +501,30 @@ class Application(tk.Frame):
 
 		self.menuFrame = tk.Frame(mainFrame, bg='green')
 		labelName = Label(self.menuFrame, text="TEMPORARY TEXT")
-		self.menuFrame.pack()
+		labelName.pack()
 		self.createMenu(self.menuFrame, self.menu_tree.tree[0], True)
 		# self.createTempMenu()
 		self.menuFrame.grid_forget()
+
+	def createMenu(self, previousMenu, clickedNode, atRoot):
+
+		if not atRoot:
+			previousMenu.grid_forget()
+		newMenu = tk.Frame(self.menuFrame, bg='red', width=750, height=400)
+		level = self.menu_tree.getSelectionLevel(clickedNode)
+		self.menu_tree.traverseDownToSelectionLevel(clickedNode)
+		rowNumber = 0 
+		for child in level:
+			if child.isLeaf():
+				settingKey = Button(newMenu, text=str('Change ')+child.name)
+				settingKey.grid(row=rowNumber, column=0)
+				settingValue = Label(newMenu, text=child.value)
+				settingValue.grid(row=rowNumber, column=1)
+			else:
+				setting = Button(newMenu, text=child.name, command=lambda : self.createMenu(newMenu,child,False))
+				setting.grid(row=rowNumber, column=0)
+			rowNumber +=1
+
 
 	def createTempMenu(self):
 
@@ -524,26 +544,6 @@ class Application(tk.Frame):
 				self.currentSelectionNode = child
 			rowNumber+=1
 		self.makeSelectedButtonColored(self.currentSelectionButton)
-
-
-	def createMenu(self, previousMenu, clickedNode, atRoot):
-
-		if not atRoot:
-			previousMenu.grid_forget()
-		newMenu = tk.Frame(self.menuFrame, bg='red', width=750, height=400)
-		level = self.menu_tree.getSelectionLevel(clickedNode)
-		self.menu_tree.traverseDownToSelectionLevel(clickedNode)
-		rowNumber = 0 
-		for child in level:
-			if child.isLeaf():
-				settingKey = Button(self.menuFrame, text=str('Change ')+child.name)
-				settingKey.grid(row=rowNumber, column=0)
-				settingValue = Label(self.menuFrame, text=child.value)
-				settingValue.grid(row=rowNumber, column=1)
-			else:
-				setting = Button(self.menuFrame, text=child.name, command=lambda : self.createMenu(newMenu,child,False))
-				setting.grid(row=rowNumber, column=0)
-			rowNumber +=1
 
 
 	def changeMenuValue(self, clickedNode, labelToChange):
