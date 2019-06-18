d2d1a9f1b8fdb0d893022c61184c257d74723783
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-18 17:16:01 -0400

    from desktop

--- UI.py
+++ UI.py
@@ -764,12 +764,12 @@ class Application(tk.Frame):
 		self.buttonCheck()
 		tempCounter +=1
 
-		# print("C")
-		# time.sleep(sleeptime)
-		# img = self.get_live_image_temp(tempCounter)
-		# self.setLiveImage(img)
-		# self.buttonCheck()
-		# tempCounter +=1
+		print("C")
+		time.sleep(sleeptime)
+		img = self.get_live_image_temp(tempCounter)
+		self.setLiveImage(img)
+		self.buttonCheck()
+		tempCounter +=1
 
 		# print("D")
 		# time.sleep(sleeptime)
