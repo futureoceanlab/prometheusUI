c6676d3e035c0509f0ee5f35944d763f20a19557
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-18 17:44:08 -0400

    from desktop

--- UI.py
+++ UI.py
@@ -753,7 +753,7 @@ class Application(tk.Frame):
 		img = self.get_live_image_temp(r)
 		self.setLiveImage(img)
 
-		self.after(3, self.rapidFireUpdate)
+		self.after(2, self.rapidFireUpdate)
 
 	def directoryCounter(self, path):
 
