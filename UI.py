f5f5b59c80d3281912cd9192dd7e4d75cdf06e47
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-20 18:23:13 -0400

    fromDesktop

--- UI.py
+++ UI.py
@@ -609,35 +609,35 @@ class Application(tk.Frame):
 
 	def createMenu2(self,frame):
 		mnu = self.makemenu(frame)
-	    mnu.config(bd=2, relief=RAISED)
-	    frm.pack(expand=YES, fill=BOTH)
-	    Label(frm, bg='black', height=5, width=15).pack(expand=YES, fill=BOTH)
+		mnu.config(bd=2, relief=RAISED)
+		frm.pack(expand=YES, fill=BOTH)
+		Label(frm, bg='black', height=5, width=15).pack(expand=YES, fill=BOTH)
 
 	def makeMenu(self,parent):
 		menubar = Frame(parent)                        
-	    menubar.pack(side=TOP, fill=X)
-	    
-	    fbutton = Menubutton(menubar, text='File', underline=0)
-	    fbutton.pack(side=LEFT)
-	    file = Menu(fbutton)
-	    file.add_command(label='New...',  command=notdone,     underline=0)
-	    file.add_command(label='Open...', command=notdone,     underline=0)
-	    file.add_command(label='Quit',    command=parent.quit, underline=0)
-	    fbutton.config(menu=file)
-	     
-	    ebutton = Menubutton(menubar, text='Edit', underline=0)
-	    ebutton.pack(side=LEFT)
-	    edit = Menu(ebutton, tearoff=0)
-	    edit.add_command(label='Cut',     command=notdone,     underline=0)
-	    edit.add_command(label='Paste',   command=notdone,     underline=0)
-	    edit.add_separator()
-	    ebutton.config(menu=edit)
-	     
-	    submenu = Menu(edit, tearoff=0)
-	    submenu.add_command(label='Spam', command=parent.quit, underline=0)
-	    submenu.add_command(label='Eggs', command=notdone,     underline=0)
-	    edit.add_cascade(label='Stuff',   menu=submenu,        underline=0)
-	    return menubar
+		menubar.pack(side=TOP, fill=X)
+		
+		fbutton = Menubutton(menubar, text='File', underline=0)
+		fbutton.pack(side=LEFT)
+		file = Menu(fbutton)
+		file.add_command(label='New...',  command=notdone,     underline=0)
+		file.add_command(label='Open...', command=notdone,     underline=0)
+		file.add_command(label='Quit',    command=parent.quit, underline=0)
+		fbutton.config(menu=file)
+		 
+		ebutton = Menubutton(menubar, text='Edit', underline=0)
+		ebutton.pack(side=LEFT)
+		edit = Menu(ebutton, tearoff=0)
+		edit.add_command(label='Cut',     command=notdone,     underline=0)
+		edit.add_command(label='Paste',   command=notdone,     underline=0)
+		edit.add_separator()
+		ebutton.config(menu=edit)
+		 
+		submenu = Menu(edit, tearoff=0)
+		submenu.add_command(label='Spam', command=parent.quit, underline=0)
+		submenu.add_command(label='Eggs', command=notdone,     underline=0)
+		edit.add_cascade(label='Stuff',   menu=submenu,        underline=0)
+		return menubar
 
 
 	def createTempMenu(self):
