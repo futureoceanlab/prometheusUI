67eef66fa11b38bbfaa1f544580f2e281bc3deb7
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-20 18:25:15 -0400

    fromDesktop

--- UI.py
+++ UI.py
@@ -612,6 +612,8 @@ class Application(tk.Frame):
 		mnu.config(bd=2, relief=RAISED)
 		frm.pack(expand=YES, fill=BOTH)
 		Label(frm, bg='black', height=5, width=15).pack(expand=YES, fill=BOTH)
+	def notdone(self):
+		return
 
 	def makeMenu(self,parent):
 		menubar = Frame(parent)                        
@@ -620,22 +622,22 @@ class Application(tk.Frame):
 		fbutton = Menubutton(menubar, text='File', underline=0)
 		fbutton.pack(side=LEFT)
 		file = Menu(fbutton)
-		file.add_command(label='New...',  command=notdone,     underline=0)
-		file.add_command(label='Open...', command=notdone,     underline=0)
+		file.add_command(label='New...',  command=self.notdone,     underline=0)
+		file.add_command(label='Open...', command=self.notdone,     underline=0)
 		file.add_command(label='Quit',    command=parent.quit, underline=0)
 		fbutton.config(menu=file)
 		 
 		ebutton = Menubutton(menubar, text='Edit', underline=0)
 		ebutton.pack(side=LEFT)
 		edit = Menu(ebutton, tearoff=0)
-		edit.add_command(label='Cut',     command=notdone,     underline=0)
-		edit.add_command(label='Paste',   command=notdone,     underline=0)
+		edit.add_command(label='Cut',     command=self.notdone,     underline=0)
+		edit.add_command(label='Paste',   command=self.notdone,     underline=0)
 		edit.add_separator()
 		ebutton.config(menu=edit)
 		 
 		submenu = Menu(edit, tearoff=0)
 		submenu.add_command(label='Spam', command=parent.quit, underline=0)
-		submenu.add_command(label='Eggs', command=notdone,     underline=0)
+		submenu.add_command(label='Eggs', command=self.notdone,     underline=0)
 		edit.add_cascade(label='Stuff',   menu=submenu,        underline=0)
 		return menubar
 
