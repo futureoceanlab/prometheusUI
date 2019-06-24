7645af00396d2ff2f49a582688eea8244bc3a8f5
Lab Laptop <futureoceanlab@gmail.com>
2019-06-24 13:40:07 -0400

    from LL

--- i2cRead.py
+++ i2cRead.py
@@ -1,7 +1,13 @@
 import smbus
 
+address = 0x18
+reg_temp = 0x00
+i2c_ch = 1
+bus = smbus.SMBus(i2c_ch)
+
 def read_temp():
 	print('testinnnnggg')
+	value = bus.read_i2c_block_data(address, reg_temp, 2)
 	return
 
 if __name__ =='__main__':
