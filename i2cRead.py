0d82443ec7ca3638a06dddc4cc19354c5e122f59
Lab Laptop <futureoceanlab@gmail.com>
2019-06-24 13:44:10 -0400

    from LL

--- i2cRead.py
+++ i2cRead.py
@@ -16,16 +16,20 @@ def read_temp():
     # Read temperature registers
     val = bus.read_i2c_block_data(address, reg_temp, 2)
     temp_c = (val[0] << 4) | (val[1] >> 5)
+    print("val: ", val)
+    print("temp_c: ", temp_c)
 
     # Convert to 2s complement (temperatures can be negative)
     temp_c = twos_comp(temp_c, 12)
+    print("twos: ", temp_c)
 
     # Convert registers value to temperature (C)
     temp_c = temp_c * 0.0625
+    print(temp_c)
 
     return temp_c
 
 
 if __name__ =='__main__':
 	while True:
-		print(read_temp())
+		read_temp()
