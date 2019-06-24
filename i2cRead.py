e4712bdcf85939bce389e034017ecb45f2bf06c0
Lab Laptop <futureoceanlab@gmail.com>
2019-06-24 13:42:50 -0400

    from LL

--- i2cRead.py
+++ i2cRead.py
@@ -5,10 +5,26 @@ reg_temp = 0x00
 i2c_ch = 1
 bus = smbus.SMBus(i2c_ch)
 
+def twos_comp(val, bits):
+    if (val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return val
+
+# Read temperature registers and calculate Celsius
 def read_temp():
-	print('testinnnnggg')
-	value = bus.read_i2c_block_data(address, reg_temp, 2)
-	return value
+
+    # Read temperature registers
+    val = bus.read_i2c_block_data(address, reg_temp, 2)
+    temp_c = (val[0] << 4) | (val[1] >> 5)
+
+    # Convert to 2s complement (temperatures can be negative)
+    temp_c = twos_comp(temp_c, 12)
+
+    # Convert registers value to temperature (C)
+    temp_c = temp_c * 0.0625
+
+    return temp_c
+
 
 if __name__ =='__main__':
 	while True:
