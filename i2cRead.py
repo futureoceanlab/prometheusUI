8d28df13aebc78cbc926204df6f3a2298b64581e
Lab Laptop <futureoceanlab@gmail.com>
2019-06-24 15:34:43 -0400

    from LL

--- i2cRead.py
+++ i2cRead.py
@@ -1,38 +1,9 @@
-import smbus
+import Adafruit_PureIO.smbus as smbus
 
 # address = 0x18
 # reg_temp = 0x00
 i2c_ch = 1
 bus = smbus.SMBus(i2c_ch)
-
-# def twos_comp(val, bits):
-#     if (val & (1 << (bits - 1))) != 0:
-#         val = val - (1 << bits)
-#     return val
-
-# # Read temperature registers and calculate Celsius
-# def read_temp():
-
-#     # Read temperature registers
-#     val = bus.read_i2c_block_data(address, reg_temp, 2)
-#     temp_c = (val[0] << 4) | (val[1] >> 5)
-#     print("val: ", val)
-#     print("temp_c: ", temp_c)
-
-#     # Convert to 2s complement (temperatures can be negative)
-#     temp_c = twos_comp(temp_c, 12)
-#     print("twos: ", temp_c)
-
-#     # Convert registers value to temperature (C)
-#     temp_c = temp_c * 0.0625
-#     print(temp_c)
-
-#     return temp_c
-
-
-# if __name__ =='__main__':
-# 	while True:
-# 		read_temp()
 
 
 import time
@@ -47,29 +18,15 @@ i2c_address = 0x18
 reg_temp = 0x00
 reg_config = 0x01
 
-# Calculate the 2's complement of a number
-def twos_comp(val, bits):
-    if (val & (1 << (bits - 1))) != 0:
-        val = val - (1 << bits)
-    return val
 
 # Read temperature registers and calculate Celsius
 def read_temp():
 
     # Read temperature registers
-    val = bus.read_i2c_block_data(i2c_address, reg_temp, 16)
-    print(val)
-    temp_c = (val[0] << 4) | (val[1] >> 5)
-
-    # Convert to 2s complement (temperatures can be negative)
-    temp_c = twos_comp(temp_c, 16)
-
-    # Convert registers value to temperature (C)
-    temp_c = temp_c * 0.0625
-
-    return temp_c
-
-
+    val = bus.readS16BE(i2c_address)
+    print("a ", val)
+    val = ((val << 8) & 0xFF00) + (val >> 8)
+    print("b ", val)
 # Print out temperature every second
 while True:
     temperature = read_temp()
