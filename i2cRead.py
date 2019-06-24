f0a0893575af39446d2ea7c26481c4893c203885
Lab Laptop <futureoceanlab@gmail.com>
2019-06-24 15:17:39 -0400

    from LL

--- i2cRead.py
+++ i2cRead.py
@@ -1,9 +1,9 @@
-import smbus
+# import smbus
 
-address = 0x18
-reg_temp = 0x00
-i2c_ch = 1
-bus = smbus.SMBus(i2c_ch)
+# address = 0x18
+# reg_temp = 0x00
+# i2c_ch = 1
+# bus = smbus.SMBus(i2c_ch)
 
 # def twos_comp(val, bits):
 #     if (val & (1 << (bits - 1))) != 0:
@@ -41,11 +41,11 @@ import smbus
 i2c_ch = 1
 
 # TMP102 address on the I2C bus
-i2c_address = 0x18
+i2c_address = 0x48
 
 # Register addresses
 reg_temp = 0x00
-reg_config = 0x00
+reg_config = 0x01
 
 # Calculate the 2's complement of a number
 def twos_comp(val, bits):
@@ -57,17 +57,14 @@ def twos_comp(val, bits):
 def read_temp():
 
     # Read temperature registers
-
-    val = bus.read_word_data(i2c_address, reg_temp) & 0xFFFF
-    val = ((val << 8) & 0xFF00) + (val >> 8)
-    # temp_c = (val[0] << 4) | (val[1] >> 5)
-    print(val)
+    val = bus.read_i2c_block_data(i2c_address, reg_temp, 2)
+    temp_c = (val[0] << 4) | (val[1] >> 5)
 
     # Convert to 2s complement (temperatures can be negative)
-    # temp_c = twos_comp(temp_c, 12)
+    temp_c = twos_comp(temp_c, 16)
 
-    # # Convert registers value to temperature (C)
-    # temp_c = temp_c * 0.0625
+    # Convert registers value to temperature (C)
+    temp_c = temp_c * 0.0625
 
     return temp_c
 
