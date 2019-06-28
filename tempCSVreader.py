c5cce9649017fc2b8320f48fcd5297ab8d22f13c
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-27 22:05:37 -0400

    from Desktop

--- tempCSVreader.py
+++ tempCSVreader.py
@@ -2,5 +2,8 @@ import csv
 with open('normDCIconvshift.csv', 'r') as f:
     reader = csv.reader(f)
     your_list = list(reader)[0]
+    newList = []
+    for i in your_list:
+    	newList.append(float(i))
 
-print(your_list)
+print(newList)
