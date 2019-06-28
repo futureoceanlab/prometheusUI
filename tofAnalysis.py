78fb95b171ab189c1afb2320d1d7a5e7198a4341
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-28 00:10:13 -0400

    from Desktop

--- tofAnalysis.py
+++ tofAnalysis.py
@@ -12,8 +12,8 @@ def analyze(dcsData, freq):
 	dcs3 = [y for x in dcsData[:,:,3] for y in x]
 	print(len(dcs0), len(dcs1))
 
-	result = list(map(lambda w,x,y,z: dcsInverse(freq,w,x,y,z), dcs0, dcs1, dcs2, dcs3))
-	print("DONE ", len(result))
+	result = list(map(lambda w,x,y,z: dcsInverse(freq,w,x,y,z), dcs0[:7680], dcs1[:7680], dcs2[:7680], dcs3[:7680]))
+	print("DONE ", result)
 	return np.array(result).reshape(320,240, order='F')
 
 def logical_intersect_index(a, b):
@@ -106,5 +106,4 @@ def dcsInverse(freq, dcs0, dcs1, dcs2=None, dcs3=None):
 
 			assert(type(wavelength) == float)
 			phase = ((section[0] + est)/wavelength)%1.0
-	
 	return phase
