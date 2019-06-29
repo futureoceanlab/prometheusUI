c3dc7ccba7fd820a35745384173e25333ce28b43
futureoceanlab <51088201+futureoceanlab@users.noreply.github.com>
2019-06-29 15:49:01 -0400

    from Desktop

--- UI.py
+++ UI.py
@@ -564,7 +564,7 @@ class Application(tk.Frame):
 		# canvas.get_tk_widget().pack()
 
 		dcsCanvas = tk.Canvas(mainFrame, width=800, height=480)
-		fourDCSImages = ImageTk.PhotoImage(Image.open(readBinary.get_4DCS_PNG(self.previousImages[self.currentPreviousImage])))
+		fourDCSImages = ImageTk.PhotoImage(Image.open(readBinary.get_4DCS_PNG(self.previousImages[self.currentPreviousImage])).resize((600,450),Image.ANTIALIAS))
 		mainFrame.fourDCSImages = fourDCSImages
 		dcsCanvas.create_image(0,0,anchor=NW, image=fourDCSImages)
 		dcsCanvas.pack()
