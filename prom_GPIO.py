485092c9349c02a1e0e2f5121c7ff535abd78e23
Lab Laptop <futureoceanlab@gmail.com>
2019-06-30 01:39:06 -0400

    intial final testing

--- prom_GPIO.py
+++ prom_GPIO.py
@@ -1,26 +1,39 @@
 
 #face buttons
-MENU_BTN = 21
-DISP_BTN = 20
-EXPO_BTN = 16
-ACTN_BTN = 12
-HDR_BTN = 0000000
+MENU_BTN = 10
+DISP_BTN = 9
+EXPO_BTN = 0
+ACTN_BTN = 11
+HDR_BTN = 0
+
+#js1
+JS1_MENU_BTN = 14
+JS1_DISP_BTN = 15
+JS1_EXPO_BTN = 18
+JS1_ACTN_BTN = 23
+JS1_HDR_BTN = 24
+
+#js2
+JS2_MENU_BTN = 5
+JS2_DISP_BTN = 6
+JS2_EXPO_BTN = 13
+JS2_ACTN_BTN = 19
+JS2_HDR_BTN = 26
 
 #BBB
-BBB_0_ctrl = 7
-BBB_1_ctrl = 8
-BBB_0_reset = 23
-BBB_1_reset = 24
+BBB_0_ctrl = 12
+BBB_1_ctrl =20
+BBB_0_reset = 16
+BBB_1_reset = 21
 
 #i2c
 I2C_DATA = 2		#never explicitly called
 I2C_CLOCK = 3		#never explicitly called
 
 #misc
-LED_CAM_SELECT = 18
-SHUTTER_OUTPUT = 21 #called only from C files
-MOD_CLOCK = 4		#called only from C files
-LED_ENABLE = 0000000
+LED_CAM_SELECT = 7
+SHUTTER_OUTPUT = 25 #called only from C files
+LED_ENABLE = 8
 
 
 #getter functions
@@ -38,6 +51,36 @@ def actnGPIO():
 
 def hdrGPIO():
 	return HDR_BTN
+
+def js1_menuGPIO():
+	return JS1_MENU_BTN
+
+def js1_dispGPIO():
+	return JS1_DISP_BTN
+
+def js1_expoGPIO():
+	return JS1_EXPO_BTN
+
+def js1_actnGPIO():
+	return JS1_ACTN_BTN
+
+def js1_hdrGPIO():
+	return JS1_HDR_BTN
+
+def js2_menuGPIO():
+	return JS2_MENU_BTN
+
+def js2_dispGPIO():
+	return JS2_DISP_BTN
+
+def js2_expoGPIO():
+	return JS2_EXPO_BTN
+
+def js2_actnGPIO():
+	return JS2_ACTN_BTN
+
+def js2_hdrGPIO():
+	return JS2_HDR_BTN
 
 def bbb0_ctrl_GPIO():
 	return BBB_0_ctrl
@@ -62,9 +105,6 @@ def led_cam_select_GPIO():
 
 def shutter_output_GPIO():
 	return SHUTTER_OUTPUT
-
-def mod_clock_GPIO():
-	return MOD_CLOCK
 
 def led_enable_GPIO():
 	return LED_ENABLE
