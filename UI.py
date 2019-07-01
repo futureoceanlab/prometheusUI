43103de101428317d47684155477090c11ab28b3
Lab Laptop <futureoceanlab@gmail.com>
2019-07-01 16:11:15 -0400

    UI fixes

--- UI.py
+++ UI.py
@@ -973,7 +973,7 @@ class Application(tk.Frame):
 			self.setCapturingVideoImage(ImageTk.PhotoImage(Image.open('video.jpg').resize((1440,950),Image.ANTIALIAS)))
 			self.change_display(5) #to live view/video capture image
 			self.update_display()
-			# self.capture_video(False)
+			self.capture_video(False)
 		else:                               
 			#else is same as short press
 			self.ACTN_short_pressed()
