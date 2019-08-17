6d30c8cc2b23a43275455a4069525271fe96bce1
Jake Bernstein <jgbernstein@gmail.com>
2019-08-17 16:17:25 -0400

    basic metadata save working

--- prometheus-cli/promCliUtil.py
+++ prometheus-cli/promCliUtil.py
@@ -7,6 +7,9 @@ Created on Fri Aug 16 19:52:17 2019
 """
 
 import os
+import csv
+from datetime import datetime
+import itertools
 
 class captureSetting:
     
@@ -33,7 +36,7 @@ class captureSetting:
         for key in currAtts:
             if not(currAtts[key] == prevAtts[key]):
                 cmdlist.append(self.attrCmd(key, currAtts[key]))
-        cmdlist.append(self.imgCmd())
+        #cmdlist.append(self.imgCmd())
         return cmdlist
     
     def attrCmd(self, key, val):
@@ -61,15 +64,22 @@ class promSession:
     
     def __init__(
             self,
-            startupfile='startupcommands.txt',
-            metadatafile='capturemetadata.csv',
+            startupfilename='startupcommands.txt',
+            metadatafilename='capturemetadata.csv',
+            outputpath= '../../images',
             buildpath='./build',
             cams=0,
             currSet = None
             ):
         self.cams = cams;
         self.cmdpath = os.path.abspath(os.path.join(buildpath,'prom-cli'))
-        self.startup(startupfile,cams)
+        self.outputpath = os.path.abspath(os.path.normpath(outputpath))
+        self.metafile = open(os.path.join(self.outputpath,metadatafilename),'a',newline='')
+        self.metawriter = csv.writer(self.metafile)            
+        self.timestamp = datetime.now().strftime('%y%m%d%H%M')
+        self.startup(startupfilename,cams)
+        self.numimages=0
+        self.numvideos=0
         if currSet is None:
             self.currSet = captureSetting()
         
@@ -77,35 +87,55 @@ class promSession:
         with open(startupfile, "r") as log:
     	    cmd = log.readline().strip('\n')
     	    while cmd:
-                self.writecommand(cmd,'hexdump')
+                self.writecommand(cmd,'| hexdump')
                 cmd = log.readline().strip('\n')
                 
-    #def shutdown(self):
+    def shutdown(self):
+        self.metafile.close()
     
     def writecommand(self,commandstring,output):
-        modcom = "{} \"{}\" -i {} | {}".format(self.cmdpath, commandstring, self.cams, output)
+        modcom = "{} \"{}\" -i {} {}".format(self.cmdpath, commandstring, self.cams, output)
         print(modcom)
         #os.system(mod_com)
         
-    def captureImage(self,capSet):
+    def captureImage(self,capSet,filename=None):        
+        #This is a bit of funky logic to allow captureHDRImage to make calls to capture Image
+        if (filename is None):
+            self.numimages = self.numimages+1
+            filename = 'image{}_{1:03d}.bin'.format(self.timestamp,self.numimages)
+        #
         cmdlist = capSet.listCmds(self.currSet)
-        self.currSet = capSet
+        imgCmd = capSet.imgCmd()        
+        self.updateCapSet(capSet)
+        #
         for cmd in cmdlist:
-            self.writecommand(cmd,'hexdump')
+            self.writecommand(cmd,'| hexdump')
+        self.writecommand(imgCmd,'> {}.bin'.format(os.path.join(self.outputpath,filename)))
+        #
+        capAtts = vars(self.currSet)
+        self.metawriter.writerow([filename, datetime.now().strftime('%H%M%S.%f)')[:-3]] + list(itertools.chain(*capAtts.items())))
+        
     
     def captureHDRImage(self,capSets):
-        for capSet in capSets:
-            self.captureImage(capSet)
+        self.numimages = self.numimages + 1
+        fileprefix = 'image{0}_{1:03d}'.format(self.timestamp,self.numimages)
+        for i in range(len(capSets)):
+            filename = fileprefix + '-{0:02d}'.format(i)
+            self.captureImage(capSets[i],filename)
             
-    #def captureVideo(self,capSet,nImag):
+    def updateCapSet(self,newcapSet):
+        capAtts = vars(newcapSet)
+        for attkey in capAtts.keys():
+            if not(capAtts[attkey] is None):
+                setattr(self.currSet,attkey,capAtts[attkey])
+                
+    def captureVideo(self,capSet,nImag):
+        for i in range(nImag):
+            self.captureImage(capSet)
         
-    #def captureHDRVideo(self,capSets,nImag):
-        
-    #def __openMeta(self):
-        
-    #def __writeMeta(self):
-        
-    #def __closeMeta(self):
+    def captureHDRVideo(self,capSets,nImag):
+        for i in range(nImag):
+            self.captureHDRImage(capSets)
         
 
     
