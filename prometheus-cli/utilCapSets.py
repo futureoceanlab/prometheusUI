7170e25299b7268c14556b7901040c3de13f0ff9
Jake Bernstein <jgbernstein@gmail.com>
2019-09-10 13:51:20 -0400

    updated utilCapSets.py

--- prometheus-cli/utilCapSets.py
+++ prometheus-cli/utilCapSets.py
@@ -1,3 +1,4 @@
+from promCliUtil import *
 capvals = {'mode': 1, 'piDelay': 1, 'exposureTime': 100, 'modFreq': 1}
 cap1 = captureSetting(**capvals)
 capvals['exposureTime'] = 300
@@ -6,4 +7,9 @@ capvals['exposureTime'] = 1000
 cap3 = captureSetting(**capvals)
 capvals['exposureTime'] = 3000
 cap4 = captureSetting(**capvals)
-capSet = [cap1, cap2, cap3, cap4]
+capvals['mode'] = 0
+capvals['exposureTime'] = 100
+cap5 = captureSetting(**capvals)
+capvals['exposureTime'] = 1000
+cap6 = captureSetting(**capvals)
+capSet = [cap1, cap2, cap3, cap4, cap5, cap6]
